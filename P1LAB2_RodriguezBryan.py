55d948be3da00b72f76368d9fbd2d1104bad82e6
BryanRoDR <143443770+BryanRoDR@users.noreply.github.com>
2023-09-20 14:14:18 -0400

    P1LAB2

--- P1LAB2_RodriguezBryan.py
+++ P1LAB2_RodriguezBryan.py
@@ -1,7 +1,7 @@
 user_num = int(input('Enter integer:\n'))
 print('You entered:', user_num)
 print(user_num, 'squared is', user_num * user_num)
-print('And', user_num, 'cubed is', user_num**3)
+print('And', user_num, 'cubed is', user_num**3, '!!')
 user_num2 = int(input('Enter another interger:\n'))
 print(user_num2)
 print(user_num, "+", user_num2, "is", user_num+user_num2)
