0895884f506a4b5e5c40f5fd0e9580b2e5cccd7c
BryanRoDR <143443770+BryanRoDR@users.noreply.github.com>
2023-09-27 14:04:02 -0400

    v1

---
+++ P2LAB2_RodriguezBryan.py
@@ -0,0 +1,14 @@
+#Bryan Rodriguez
+#9/27/2023
+#Using Floats for Calculation
+
+num1 = float(input("Enter A Float: "))
+num2 = float(input("Enter A Float: "))
+num3 = float(input("Enter A Float: "))
+num4 = float(input("Enter A Float: "))
+
+print(f'The Product of the Number is: {(num1*num2*num3*num4):.0f}')
+print(f'The Product of the Number is: {(num1*num2*num3*num4):.2f}')
+average = (num1+num2+num3+num4)/4
+print(f'The Average of the Number is: {(average):.0f}')
+print(f'The Average of the Number is: {(average):.2f}')
